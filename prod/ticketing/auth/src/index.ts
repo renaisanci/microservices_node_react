0d1f9a83de9b7827eb77957f5c6d52f6438d35f9
Renato Fonseca <renaisanci@gmail.com>
2020-09-07 15:09:34 -0300

    set up jet tests and created authentication tests

--- prod/ticketing/auth/src/index.ts
+++ prod/ticketing/auth/src/index.ts
@@ -1,64 +1,26 @@
-import { NotFoundError } from './errors/not-found-error';
-import express from 'express';
-import { json } from 'body-parser';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/signup'
-import { errorHandler } from './middlewares/error-handler'
-
-const app = express();
-app.use(json());
-
-app.set('trust proxy',true);
-
-app.use(
-cookieSession({
-  signed: false,
-  secure:true
-})
-
-)
-
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi Renato')
-})
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-
-app.get('*', async (req, res) => {
-  throw new NotFoundError();
-})
-
-app.use(errorHandler);
-
+import { app } from './app';
 
 const start = async () => {
-  try {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
 
+  try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
     });
-    console.log('connected mongo DB !');
+    console.log('Connected to MongoDb');
   } catch (err) {
-
-    console.error(err)
+    console.error(err);
   }
-};
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000 !!!!');
-});
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
 
 start();
--- prod/ticketing/auth/src/routes/current-user.ts
+++ prod/ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
 import express from 'express';
- 
+import { currentUser } from '../middlewares/current-user';
+
 const router = express.Router();
 
-router.get('/api/users/current', (req, res) => {
-res.send('Hi current user')
+router.get('/api/users/currentuser', currentUser,  (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
